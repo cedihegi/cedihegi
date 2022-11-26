5ed0082bd6d16d6b31144894c6b7aa2aa73141f2
hegglinc <hegglinc@student.ethz.ch>
2022-11-26 03:54:11 +0100

    setup script update

---
+++ i3/setup.sh
@@ -0,0 +1,17 @@
+# just a file so I will remember all the necessary steps to get 
+# this config running on a different pc
+
+# install i3 gaps
+sudo apt update
+
+sudo add-apt-repository -y ppa:regolith-linux/stable
+
+sudo apt install i3-gaps
+sudo apt install i3
+sudo apt install feh
+sudo apt install fonts-powerline 
+
+# we also need bumblebee to be installed!
+echo "Cloning Bumblebee into ~/Sources/bumblebee-status"
+
+git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git /home/cedric/Sources/bumblebee-status
